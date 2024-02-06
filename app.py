603b8561533e3966d59fd1519e1c6e704117a54e
Sachh Moka <sachhmoka@gmail.com>
2024-02-06 10:29:31 +1000

    Add cifar example

--- app.py
+++ app.py
@@ -7,3 +7,16 @@ import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
+
+from datasets import load_dataset
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+dataset_train = load_dataset(
+    'cifar10',
+    split='train',
+    ignore_verifications=True  # set to True if seeing splits Error
+)
+
+print(dataset_train)
